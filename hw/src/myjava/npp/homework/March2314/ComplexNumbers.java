6c980b4fd9b1b0790b36def088c3ee0e0ce7d30c
=Chihirev <=olegchihirev@yandex.ru>
2014-03-28 23:29:11 +0400

    update

--- hw/src/myjava/npp/homework/March2314/ComplexNumbers.java
+++ hw/src/myjava/npp/homework/March2314/ComplexNumbers.java
@@ -1,62 +1,53 @@
 package myjava.npp.homework.March2314;
-import java.util.Random;
 import java.lang.Math;
 /**
  * Created by Zahvat on 23.03.14.
  */
-public class ComplexNumbers {
+public class ComplexNumbers  implements ComplexActions, ComplexMath, TrigonometricForm{
     private double a, b;
-   ComplexNumbers()
-   {
-       Random rnd=new Random();
-       this.a=rnd.nextDouble();
-       this.b=rnd.nextDouble();
+    private final int c=0;
+   ComplexNumbers(){
+
+       this.a=c;
+       this.b=c;
    }
-    ComplexNumbers(double a, double b)
-    {
+
+    ComplexNumbers(double a, double b) {
         this.a=a;
         this.b=b;
     }
-    public String toString()
-    {
+    public String toString(){
         if(b>0)
         return(a+" + i"+b);
             return(a+" - i"+(-b));
     }
-     public boolean equals(ComplexNumbers Complex)
-     {
+     public boolean equals(ComplexNumbers Complex){
          if(this.a==Complex.a && this.b==Complex.b)
              return true;
          return false;
      }
-    public double getModule()
-    {
+    public double getModule(){
         return Math.sqrt(this.a*this.a+this.b*this.b);
     }
-    public double getArg()
-    {
+    public double getArg(){
         if(this.a>0)
             return (Math.atan(this.b/this.a));
         if(this.a<0 && this.b>0)
             return (Math.PI+Math.atan(this.b/this.a));
         return (Math.atan(this.b/this.a)-Math.PI);
     }
-    public void conjugate()
-    {
+    public void conjugate(){
        this.b*=-1;
     }
-    public void sum(ComplexNumbers Complex)
-    {
+    public void sum(ComplexNumbers Complex){
         this.a+=Complex.a;
         this.b+=Complex.b;
     }
-    public void difference(ComplexNumbers Complex)
-    {
+    public void difference(ComplexNumbers Complex){
         this.a-=Complex.a;
         this.b-=Complex.b;
     }
-    public void productOnNumber(double number)
-    {
+    public void productOnNumber(double number){
         this.a*=number;
         this.b*=number;
     }
@@ -68,8 +59,7 @@ public class ComplexNumbers {
     {
         return this.a;
     }
-    public void setValueOfImAndRe(double a, double b)
-    {
+    public void setValueOfImAndRe(double a, double b){
         this.a=a;
         this.b=b;
     }
